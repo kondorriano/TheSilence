9283ba043603546f798d632949fd2d36d353e555
kondorriano <nicobito@gmail.com>
2017-03-06 16:37:57 +0100

    Cabin interior and bass control, no vr

---
+++ Assets/Scripts/ArmsCabinController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmsCabinController : MonoBehaviour {
+
+    public Transform rightController;
+    public Transform leftController;
+    public Transform rightHand;
+    public Transform leftHand;
+
+    public float scaling = 3;
+
+	// Update is called once per frame
+	void Update () {
+        rightHand.localPosition = rightController.localPosition * scaling;
+        leftHand.localPosition = leftController.localPosition * scaling;
+    }
+}
