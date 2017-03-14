3e63bb17249557624cb186966428e7dab3ed8889
kondorriano <nicobito@gmail.com>
2017-03-14 15:04:19 +0100

    Melody and stuff

--- Assets/Scripts/KillBombOnTrigger.cs
+++ Assets/Scripts/KillBombOnTrigger.cs
@@ -6,7 +6,6 @@ public class KillBombOnTrigger : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
         if (other.tag == "Bomb") Destroy(other.gameObject);
     }
 }
